3625b7a6af0fb07aab0bd4786d44d313ad8e5c5c
Alex Richason <aarichason@csuchico.edu>
2023-02-21 13:43:33 -0800

    sidebar started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 //import { navigationContainer} from '@react-navigation/native'
+import './App.css';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/Homescreen.js';
@@ -6,6 +7,8 @@ import Posting from './screens/Postings.js';
 import React from 'react';
 //import {Text} from 'react-native'
 import { View, Text} from 'react-native';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 
@@ -21,14 +24,35 @@ const stack = createStackNavigator();
 //   )
 // }
 
+      // <NavigationContainer>
+      // <stack.Navigator>
+      //   <stack.Screen name = "Homescreen" component = {HomeScreen} />
+      //   <stack.Screen name = "listings" component = {Posting} />
+      // </stack.Navigator>
+      // </NavigationContainer>
+//this would be in <>
+      // <Router>
+      //   <Navbar />
+      //   <switch>
+      //     <Route path='/' />
+      //   </switch>
+      // </Router>
 const App = () =>{
   return(
-    <NavigationContainer>
-    <stack.Navigator>
-      <stack.Screen name = "Homescreen" component = {HomeScreen} />
-      <stack.Screen name = "listings" component = {Posting} />
-    </stack.Navigator>
-    </NavigationContainer>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' />
+        </Routes>
+      </Router>
+      <NavigationContainer>
+      <stack.Navigator>
+        <stack.Screen name = "Homescreen" component = {HomeScreen} />
+        <stack.Screen name = "listings" component = {Posting} />
+      </stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
