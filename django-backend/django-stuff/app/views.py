fcfe9af0d0ed332f9ba1dd74d30f50c71d62640c
Travis Kroll <tjkroll@mail.csuchico.edu>
2023-02-22 17:05:38 -0800

    added urls for frontend to reference and set up basic view templates

--- django-backend/django-stuff/app/views.py
+++ django-backend/django-stuff/app/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, response
 
-# Create your views here.
+
+#functions not flushed out and neither are models
+
+
+
+# This returns all the listings
+def GetEverything(request):
+    dmp = {}
+    dmp = post_model.filter.all()
+    return response({"listings": json.dumps(dmp)})
+
+# This saves the new user
+def SaveUser(request):
+    data = request.content
+    user_model.save(data.user, data.name, data.password)
+    return response({"result":"success"})
+
+# This takes the users post info and saves it to the database
+def SavePost(request):
+    data = request.content
+    post_model.save(data.blah, data.blah, data.blah)
+    return response({"result":"success"})
+
+# This will return all the posts for a specific user
+def UserPosts(request):
+    dmp = {}
+    dmp = post_model.filter(user = request.user)
+
+    return response({"user_posts": json.dumps(dmp)})
