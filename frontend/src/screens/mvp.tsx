c20450aec506f4f968c8c85e6c754d9f1cf09fda
keoni Burns <keoni.burns@gmail.com>
2023-03-06 16:51:56 -0800

    we can display json data on our page now

--- frontend/src/screens/mvp.tsx
+++ frontend/src/screens/mvp.tsx
@@ -9,40 +9,19 @@ import {
   View,
   TextBase,
 } from 'react-native';
+import {useEffect} from 'react';
+import {LogBox} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
-import {useState, useEffect} from 'react';
-import axios from 'axios';
-import {Card} from 'react-native-paper';
-import {FAB} from '@rneui/themed';
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/Ionicons';
-import {LogBox} from 'react-native';
+
+import DisplayData from '../../components/displayData';
 
 function MVP(): JSX.Element {
   const navigation = useNavigation();
-  const [myData, setData] = useState({details: []});
-  useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/todos/1')
-      .then(res => {
-        setData({details: res.data});
-      })
-      .catch(err => {
-        console.log('no data');
-      });
-    //console.log(myData);
-  }, []);
-  console.log(myData);
-
   useEffect(() => {
     LogBox.ignoreLogs(['Animated: `useNativeDriver`']);
   }, []);
-
-  // const fakeData = [
-  //   {id: 1, title: 'this is', description: 'fake data'},
-  //   {id: 2, title: 'this is', description: 'fake data'},
-  //   {id: 3, title: 'this is', description: 'fake data'},
-  // ];
 
   return (
     <SafeAreaView style={styles.container}>
@@ -55,87 +34,7 @@ function MVP(): JSX.Element {
         </ActionButton.Item>
       </ActionButton>
       <ScrollView style={styles.scroll}>
-        {/* <Text style={styles.testText}>welcome to neighborhood deals</Text> */}
-
-        <Card style={styles.card}>
-          <Card.Title title="Location" />
-          <Text>description</Text>
-          <Text>image</Text>
-        </Card>
-
-        <Card style={styles.card}>
-          <Card.Title title="fake data 2" />
-          <Text>this will be a decription eventually</Text>
-          <Text>this will be an image</Text>
-        </Card>
-
-        <Card style={styles.card}>
-          <Card.Title title="fake data 3" />
-          <Text>this will be a decription eventually</Text>
-          <Text>this will be an image</Text>
-        </Card>
-
-        <Card style={styles.card}>
-          <Card.Title title="fake data 4" />
-          <Text>this will be a decription eventually</Text>
-          <Text>this will be an image</Text>
-        </Card>
-
-        <Card style={styles.card}>
-          <Card.Title title="fake data 5" />
-          <Text>this will be a decription eventually</Text>
-          <Text>this will be an image</Text>
-        </Card>
-
-        <Card style={styles.card}>
-          <Card.Title title="fake data 6" />
-          <Text>this will be a decription eventually</Text>
-          <Text>this will be an image</Text>
-        </Card>
-        <Card style={styles.card}>
-          <Card.Title title="fake data 3" />
-          <Text>this will be a decription eventually</Text>
-          <Text>this will be an image</Text>
-        </Card>
-
-        <Card style={styles.card}>
-          <Card.Title title="fake data 4" />
-          <Text>this will be a decription eventually</Text>
-          <Text>this will be an image</Text>
-        </Card>
-
-        <Card style={styles.card}>
-          <Card.Title title="fake data 5" />
-          <Text>this will be a decription eventually</Text>
-          <Text>this will be an image</Text>
-        </Card>
-
-        <Card style={styles.card}>
-          <Card.Title title="fake data 6" />
-          <Text>this will be a decription eventually</Text>
-          <Text>this will be an image</Text>
-        </Card>
-        {/* 
-        <Button title="grab some data" onPress={() => console.log(myData)} />
-        <Button
-          title="display the data"
-          onPress={() => console.log('figure out how to display on page')}
-          //  onPress = {()=>console.log("nothing to see here")}
-        /> */}
-        {/* <FAB //we can make this one a plus that follows as you scroll
-          icon={{name: 'add', color: 'white'}}
-          color="yellow"
-          onPress={() => navigation.navigate('post' as never)}
-          placement="right"
-        /> */}
-        {/* <FAB
-          visible={visible}
-          onPress={() => setVisible(!visible)}
-          placement="right"
-          title="Hide"
-          icon={{name: 'delete', color: 'white'}}
-          color="red"
-        /> */}
+        <DisplayData></DisplayData>
       </ScrollView>
     </SafeAreaView>
   );
