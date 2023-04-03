90e6c326acff58f1a75c2ae44226768ebe595c2c
ZenoPsycho <slvjordan2626@gmail.com>
2023-04-03 13:59:40 -0700

    Started implementing the displayMap function

--- frontend/components/displayData.tsx
+++ frontend/components/displayData.tsx
@@ -4,6 +4,26 @@ import data from '../fakeData.json';
 import {Card, Text} from 'react-native-paper';
 import axios from 'axios';
 import {useState, useEffect} from 'react';
+import MapView from 'react-native-maps';
+
+function DisplayMap(){
+
+  //This will be the starting location for the mapp
+  <MapView
+    initialRegion={{
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }}
+  />
+
+  //This will later allow us to place markers on the map for hotspots
+  // <Marker
+  // coordinate={{latitude: latitude, longitude: longitude}}
+  // image={{uri: 'custom_pin'}}
+  // />
+}
 
 function DisplayData(): JSX.Element {
   const [myData, setData] = useState({details: []});
