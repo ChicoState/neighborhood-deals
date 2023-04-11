b82018e9bc45a611505aa17a1a6756de95f13c37
ZenoPsycho <slvjordan2626@gmail.com>
2023-04-11 10:29:04 -0700

    Added API key to android xml and removed displayMap.tsx

--- frontend/src/screens/map.tsx
+++ frontend/src/screens/map.tsx
@@ -14,11 +14,20 @@ import {LogBox} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/Ionicons';
+import MapView from 'react-native-maps';
 
 function Map(): JSX.Element {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
+        <MapView
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
         <Text>this will be a map lmao</Text>
       </ScrollView>
     </SafeAreaView>
